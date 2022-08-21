6e8f12caa339ee2b8eac343d997e604946bc1bf7
Sean Dillon <pdillon0@gmail.com>
2022-08-21 15:23:54 -0400

    prompts

--- db/schema.sql
+++ db/schema.sql
@@ -12,17 +12,15 @@ CREATE TABLE role (
 id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 title VARCHAR(30) NOT NULL,
 salary DECIMAL NOT NULL,
-department_id INT NOT NULL
-FOREIGN KEY (department_id)
-REFERENCES department(id)
+department_id INT NOT NULL,
+FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
 CREATE TABLE employee (
 id INT AUTO_INCREMENT PRIMARY KEY,
 first_name VARCHAR(30) NOT NULL,
-last_name VARCHAR(),
+last_name VARCHAR(30),
+manager_name VARCHAR(70),
 role_id INT NOT NULL,
-manager_id INT,
-FOREIGN KEY (role_id)
-REFERENCES role(id)
+FOREIGN KEY (role_id) REFERENCES role(id)
 );
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const inquirer = require('inquirer');
 const mysql = require('mysql2');
-const manageChoices = [
+const choices = [
     "View Departments",
     "View Roles",
     "View Employees",
@@ -8,6 +8,8 @@ const manageChoices = [
     "Add Role",
     "Add Employee"
 ]
+
+const managerArray = ['None']
 
 const db = mysql.createConnection(
     {
@@ -19,3 +21,107 @@ const db = mysql.createConnection(
     console.log(`work_db database`)
 );
 
+function init() {
+
+    inquirer
+    .prompt([
+        {
+            type: 'list',
+            message: "What would you like to do?",
+            choices: choices,
+            name: 'choices'
+        }
+    ]).then((data) => {
+    if(data.choices === choices[0]) {
+        console.log('View Departments');
+        return init();
+    } else if(data.choices === choices[1]) {
+        console.log('View Roles');
+        return init();
+    } else if(data.choices === choices[2]) {
+        console.log('View Employees');
+        return init();
+    } else if(data.choices === choices[3]) {
+        addDepartment();
+    } else if(data.choices === choices[4]) {
+        addRole();
+    } else if(data.choices === choices[5]) {
+        addEmployee();
+    }
+    }) 
+}
+
+function addDepartment () {
+
+    inquirer
+    .prompt([
+        {
+            type: 'input',
+            message: "What is the name of the Department?",
+            name: 'depName'
+        }
+    ]).then((data) => {
+     console.log(data);
+     return init();
+    })
+};
+
+function addRole () {
+
+    inquirer
+    .prompt([
+        {
+            type: 'input',
+            message: "What is the name of the Role?",
+            name: 'roleName'
+        },
+        {
+            type: 'input',
+            message: "What is the salary for the Role?",
+            name: 'roleSalary'
+        },
+        {
+            type: 'input',
+            message: "What is the department of this Role?",
+            name: 'roleDep'
+        }
+    ]).then((data) =>{
+     console.log(data);
+     return init
+    })
+};
+
+function addEmployee() {
+
+    inquirer
+    .prompt([
+        {
+            type: 'input',
+            message: "What is the Employees First Name?",
+            name: 'firstName'
+        },
+        {
+            type: 'input',
+            message: "What is the Employees Last Name?",
+            name: 'lastName'
+        },
+        {
+            type: 'input',
+            message: "What is the Employees Role?",
+            name: 'empRole'
+        },
+        {
+            type: 'list',
+            message: "Who is the Employees Manager?",
+            choices: managerArray,
+            name: 'empMgr'
+        }
+    ]).then((data) => {
+     console.log(data);
+     managerArray.push(data.firstName);
+     return init()
+    })
+
+};
+
+init();
