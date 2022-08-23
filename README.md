ece8e5c1454fedfc5dff14489b08149217922416
Sean Dillon <pdillon0@gmail.com>
2022-08-22 21:41:37 -0400

    initial release

---
+++ README.md
@@ -0,0 +1,24 @@
+# Employee Tracker
+  ## Table of Contents:
+  1. [Description](#description) 
+  2. [Installation](#installation)
+  3. [Usage](#usage)  
+  4. [Contributing](#contribution)
+  5. [Tests](#test)
+  6. [License](#license)
+  7. [GitHub](#github)
+  8. [E-mail](#e-mail)
+## Description
+This app allows you to view and edit a workplace database. There are three tables, for departments, roles, and employees, each referencing eachother. This app is powered by inquire and requires node.js to function.  
+## Installation
+After downloading or cloning this app from github, and after ensuring node.js is installed, open the app in your terminal and run the following commands. npm install, npm install console.table, npm install express, npm install inquirer@8.2.4, npm install mysql2, mysql -u root -p, source schema.sql, quit. To run the app, run node index.js. To clear  the default database, delete seeds.sql.
+## Usage
+https://www.youtube.com/watch?v=zTNS3bOcgK0
+## Contribution
+Feel free to fork
+## Test
+by cloning from the rep
+## License
+This Project is Unlicensed. None
+## GitHub
+https://github.com/DeviousDoge
--- index.js
+++ index.js
@@ -1,15 +1,6 @@
 const inquirer = require('inquirer');
 const mysql = require('mysql2');
-const choices = [
-    "View Departments",
-    "View Roles",
-    "View Employees",
-    "Add Department",
-    "Add Role",
-    "Add Employee"
-]
-
-
+//reference database
 const db = mysql.createConnection(
     {
         host: 'localhost',
@@ -19,34 +10,60 @@ const db = mysql.createConnection(
     },
     console.log(`work_db database`)
 );
-
+//app initialization referencepoint
 function init() {
-
+    //main menu
     inquirer
         .prompt([
             {
                 type: 'list',
                 message: "What would you like to do?",
-                choices: choices,
-                name: 'choices'
+                name: 'choices',
+                choices: [
+                    "View Departments",
+                    "View Roles",
+                    "View Employees",
+                    "Add Department",
+                    "Add Role",
+                    "Add Employee",
+                    "End"
+                ]
             }
-        ]).then((data) => {
-            if (data.choices === choices[0]) {
-                viewDepartment();
-            } else if (data.choices === choices[1]) {
-                viewRoles();
-            } else if (data.choices === choices[2]) {
-                viewEmployees();
-            } else if (data.choices === choices[3]) {
-                addDepartment();
-            } else if (data.choices === choices[4]) {
-                addRole();
-            } else if (data.choices === choices[5]) {
-                addEmployee();
+        ]).then(function ({choices})  {
+            switch (choices) {
+                case "View Departments":
+                    viewDepartment();
+                    break;
+               
+                case "View Roles": 
+                    viewRoles();
+                    break;
+                
+                case "View Employees":
+                    viewEmployees();
+                    init();
+                    break;
+            
+                case "Add Department":
+                    addDepartment();
+                    break;
+                
+                case "Add Role":
+                    addRole();
+                    break;
+
+                case "Add Employee":
+                    addEmployee();
+                    break;
+
+                case "End":
+                    db.end();
+                    break;
             }
+        
         })
 }
-
+//display department table
 function viewDepartment() {
 
     var query =
@@ -56,12 +73,11 @@ function viewDepartment() {
         if (err) throw err;
 
         console.table(res);
-        console.log("Viewing departments by id\n");
-
+        console.log("Viewing departments by id. Press down to continue.\n");
         init();
     });
 }
-
+//display roles table. Reference department table. 
 function viewRoles() {
 
     var query =
@@ -81,6 +97,7 @@ function viewRoles() {
     });
 }
 
+//display employees table. Reference roles and departments tables.
 function viewEmployees() {
 
     var query =
@@ -97,15 +114,14 @@ function viewEmployees() {
         if (err) throw err;
 
         console.table(res);
-        console.log("Viewing Employees!\n");
+        console.log("Viewing Employees! Press Down if you just selected to view employees.\n");
 
-        init();
+       
     });
 
 }
-
+//add department based on department name
 function addDepartment() {
-
     inquirer
         .prompt([
             {
@@ -113,15 +129,45 @@ function addDepartment() {
                 message: "What is the name of the Department?",
                 name: 'depName'
             }
-        ]).then((data) => {
-            console.log(data);
-            return init();
-        })
+        ]).then(function (answer) {
+            console.log(answer);
+      
+            var query = `INSERT INTO department SET ?`
+                db.query(query,
+              {
+                department_name: answer.depName,
+              },
+              function (err, res) {
+                if (err) throw err;
+      
+                console.table(res);
+                console.log("Department Added!\n");
+      
+                init();
+              });
+          })
 };
 
-
+//reference departments table and add role based on roleprompt
 function addRole() {
-
+    var query =
+      `SELECT id, department_name FROM department `
+  
+      db.query(query, function (err, res) {
+      if (err) throw err;
+  
+      const depChoices = res.map(({ id, department_name }) => ({
+        value: id, department_name
+      }));
+  
+      console.table(res);
+      console.log("Reference Point for departments");
+  
+      rolePrompt(depChoices);
+    });
+};
+//add role based on title, salary, id
+function rolePrompt() {
     inquirer
         .prompt([
             {
@@ -136,46 +182,98 @@ function addRole() {
             },
             {
                 type: 'input',
-                message: "What is the department of this Role?",
+                message: "What is the department ID of this Role?",
                 name: 'roleDep'
             }
-        ]).then((data) => {
-            console.log(data);
-            return init
-        })
-};
-
+        ]).then(function (answer) {
+            console.log(answer);
+      
+            var query = `INSERT INTO roles SET ?`
+                db.query(query,
+              {
+                title: answer.roleName,
+                salary: answer.roleSalary,
+                department_id: answer.roleDep,
+              },
+              function (err, res) {
+                if (err) throw err;
+      
+                console.table(res);
+                console.log("Role Added!\n");
+      
+                init();
+              });
+          })
+        }
+//reference roles and employee table, prompt user for employee criteria. 
 function addEmployee() {
-
+    viewEmployees();
+    var query =
+      `SELECT id, title, salary FROM roles `
+  
+      db.query(query, function (err, res) {
+      if (err) throw err;
+  
+      const roleChoices = res.map(({ id, title, salary }) => ({
+        value: id, title: `${title}`, salary: `${salary}`
+      }));
+  
+      console.table(res);
+      console.log("Reference Points for Roles and Managers(Look up...)");
+  
+      employeePrompt(roleChoices);
+    });
+  }
+  
+  //add employee based on first, last name, role, and manager. 
+  function employeePrompt(roleChoices) {
+  
     inquirer
-        .prompt([
-            {
-                type: 'input',
-                message: "What is the Employees First Name?",
-                name: 'firstName'
-            },
-            {
-                type: 'input',
-                message: "What is the Employees Last Name?",
-                name: 'lastName'
-            },
-            {
-                type: 'input',
-                message: "What is the Employees Role?",
-                name: 'empRole'
-            },
-            {
-                type: 'list',
-                message: "Who is the Employees Manager?",
-                choices: managerArray,
-                name: 'empMgr'
-            }
-        ]).then((data) => {
-            console.log(data);
-            managerArray.push(data.firstName);
-            return init()
-        })
+    .prompt([
+        {
+            type: 'input',
+            message: "What is the Employees First Name?",
+            name: 'firstName'
+        },
+        {
+            type: 'input',
+            message: "What is the Employees Last Name?",
+            name: 'lastName'
+        },
+        {
+            type: "list",
+            name: "roleId",
+            message: "What is the employee's role?",
+            choices: roleChoices
+        },
+        {
+            type: 'input',
+            message: "Please enter the Employees Manager by EMPLOYEE ID. ENTER if null.",
+            name: 'managerId'
+        },
+      ])
+      .then(function (answer) {
+        console.log(answer);
+  
+        var query = `INSERT INTO employee SET ?`
 
-};
-
+            db.query(query,
+          {
+            first_name: answer.firstName,
+            last_name: answer.lastName,
+            role_id: answer.roleId,
+            manager_id: answer.managerId,
+          },
+          function (err, res) {
+            if (err) throw err;
+  
+            console.table(res);
+            console.log("Employee Added!\n");
+  
+            init();
+          });
+      });
+  }
+  
+//initialize app.
 init();
